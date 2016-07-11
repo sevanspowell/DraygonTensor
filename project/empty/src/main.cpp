ecfc81d9187580ba26600ffbd660100eb6c696ab
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2016-07-11 09:17:25 +0800

    Add utility projects
    - Added two projects which serve as helper/example projects.
    - Removed old projects from CMake files.

---
+++ project/empty/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "engine/Engine.h"
+#include "engine/system/script/Script.h"
+#include "engine/system/platform/Platform.h"
+#include "engine/system/input/Input.h"
+#include "engine/system/render/Render.h"
+#include "engine/system/physics/Physics.h"
+
+int main(int argc, char **argv)
+{
+    ds::Engine engine;
+
+    // Create script system
+    ds::Script *scriptSystem = new ds::Script();
+
+    // Create other systems
+    ds::ISystem *inputSystem = new ds::Input();
+    ds::ISystem *renderSystem = new ds::Render();
+    ds::ISystem *physicsSystem = new ds::Physics();
+    // Register script bindings of other systems
+    scriptSystem->RegisterScriptBindings("Input", inputSystem);
+    scriptSystem->RegisterScriptBindings("Render", renderSystem);
+    scriptSystem->RegisterScriptBindings("Physics", physicsSystem);
+
+    // Add all systems to engine
+    engine.AddSystem(std::unique_ptr<ds::ISystem>(inputSystem));
+    engine.AddSystem(std::unique_ptr<ds::ISystem>(renderSystem));
+    engine.AddSystem(std::unique_ptr<ds::ISystem>(physicsSystem));
+    engine.AddSystem(std::unique_ptr<ds::ISystem>(scriptSystem));
+
+    engine.Start();
+
+    return 0;
+}
